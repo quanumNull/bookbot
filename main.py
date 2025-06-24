f5173069d84b140ab0fdc09dd922101a21d62c4b
quantumNull <brucehale_999@yahoo.com>
2025-06-24 17:23:18 -0500

    Final project: add CLI support and new books

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import sys
+from stats import get_num_words, get_char_count, sort_characters
+
 def get_book_text(path):
-    with open(path, "r", encoding="utf-8") as f:
+    with open(path) as f:
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    text = get_book_text(path)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+
+    num_words = get_num_words(text)
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    char_counts = get_char_count(text)
+    sorted_chars = sort_characters(char_counts)
+
+    print("--------- Character Count -------")
+    for entry in sorted_chars:
+        print(f"{entry['char']}: {entry['num']}")
+
+    print("============= END ===============")
 
 main()
---
+++ stats.py
@@ -0,0 +1,22 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_char_count(text):
+    text = text.lower()
+    chars = {}
+    for char in text:
+        if char in text:
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
+    return chars
+
+def sort_characters(char_dict):
+    sorted_list = []
+    for char, count in char_dict.items():
+        if char.isalpha():  # Skip non-letter characters
+            sorted_list.append({"char": char, "num": count})
+    sorted_list.sort(key=lambda x: x["num"], reverse=True)
+    return sorted_list
